9ca12ee3f3106f6bc45dddf5aa47b2fd1daab98c
y.kuki <y.kuki@grv.jpn.com>
2019-03-01 13:02:43 +0900

    viewからtemplateにパラメータを渡して表示

--- hello/views.py
+++ hello/views.py
@@ -6,4 +6,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'hello/index.html')
+    params = {
+            'title' : 'Hello/Index',
+            'msg' : 'This is sample page.',
+        }
+    return render(request, 'hello/index.html', params)
