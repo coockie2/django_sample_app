3b28f986698367077f70e4c2198b47c4d80e03b7
y.kuki <y.kuki@grv.jpn.com>
2019-03-01 11:57:32 +0900

    viewにtemplateを使用して画面に表示するように修正

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        result = 'you typed: "' + msg + '".'
-    else:
-        result = 'please send msg parameter!'
-    return HttpResponse(result)
+    return render(request, 'hello/index.html')
