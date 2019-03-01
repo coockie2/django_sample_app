b241d457839fc7da30370a6f7e1376a14c114187
y.kuki <y.kuki@grv.jpn.com>
2019-03-01 11:09:00 +0900

    ルート定義の分割

--- django_app/urls.py
+++ django_app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-import hello.views as hello
+from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/', hello.index),
+    path(r'^hello/', include('hello.urls')),
 ]
