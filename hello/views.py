25cbe2c9239e4365f4d2a8db43bea96609b8033c
y.kuki <y.kuki@grv.jpn.com>
2019-03-01 16:02:53 +0900

    編集画面・処理、削除画面・処理の追加

--- hello/views.py
+++ hello/views.py
@@ -18,9 +18,32 @@ def create(request):
     if (request.method == 'POST'):
         FriendForm(request.POST, instance = Friend()).save()
         return redirect(to='hello/')
-
     params = {
             'title' : 'Hello',
             'form' : FriendForm(),
         }
     return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id = num)
+    if (request.method == 'POST'):
+        FriendForm(request.POST, instance = obj).save()
+        return redirect(to='/hello')
+    params = {
+            'title' : 'Hello',
+            'id': num,
+            'form' : FriendForm(instance = obj),
+        }
+    return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id = num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+            'title' : 'Hello',
+            'id': num,
+            'obj' : friend,
+        }
+    return render(request, 'hello/delete.html', params)
