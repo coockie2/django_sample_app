5f7eba5540a81154411ecfcf3cedd98cc8a73d6d
y.kuki <y.kuki@grv.jpn.com>
2019-03-01 15:03:31 +0900

    IDで検索できるように修正

--- hello/views.py
+++ hello/views.py
@@ -4,12 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Friend
+from .forms import HelloForm
 
 def index(request):
-    data = Friend.objects.all()
     params = {
             'title' : 'Hello',
             'message' : 'all friends',
-            'data' : data,
+            'form' : HelloForm(),
+            'data' : [],
         }
+    if (request.method == 'POST'):
+        num = request.POST['id']
+        item = Friend.objects.get(id = num)
+        params['data'] = [item]
+        params['form'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
