941fb882e15f47a9bbb8f74e47c634c9f6e3c272
y.kuki <y.kuki@grv.jpn.com>
2019-03-01 13:12:36 +0900

    新規画面の作成、相互リンクで遷移するように修正

--- hello/views.py
+++ hello/views.py
@@ -6,8 +6,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    print 'hello#index'
     params = {
             'title' : 'Hello/Index',
-            'msg' : 'This is sample page.',
+            'msg' : 'これは、サンプルで作ったページです。',
+            'goto' : 'next',
+        }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    print 'hello#next'
+    params = {
+            'title' : 'Hello/Next',
+            'msg' : 'これは、もう1つのページです。',
+            'goto' : 'index',
         }
     return render(request, 'hello/index.html', params)
